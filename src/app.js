323498586b8c8797e1cbda40c0854cae909b74fd
akishoreS <aryasingh0127@gmail.com>
2024-05-01 03:14:12 +0530

    Adding remaining routes(healtcheck/tweetRouter/videoRouter/subscriptionRouter,etc...)

--- src/app.js
+++ src/app.js
@@ -17,9 +17,21 @@ app.use(cookieParser())
 
 //routes
 import userRouter from './routes/user.routes.js'
-
+import healCheckRouter from './routes/healthCheck.routes.js'
+import tweetRouter from './routes/tweet.routes.js'
+import videoRouter from './routes/video.routes.js'
+import subscriptionRouter from './routes/subscription.routes.js'
+import playlistRouter from './routes/playlist.routes.js'
+import likeRouter from './routes/like.routes.js'
+import commentRouter from './routes/comment.routes.js'
 //routes declaration
 app.use("/api/v1/users",userRouter)
-
+app.use("/api/v1/tweets",tweetRouter)
+app.use("/api/v1/videos",videoRouter)
+app.use("/api/v1/subscribe",subscriptionRouter)
+app.use("/api/v1/playlist",playlistRouter)
+app.use("/api/v1/Likes",likeRouter)
+app.use("/api/v1/comment",commentRouter)
+app.use("/api/v1/healthCheck",healCheckRouter)
 
 export{app}
